feddccd33a07161c5916eb59d9f45220898f5c19
Heran-Am <149790376+Heran-Am@users.noreply.github.com>
2023-11-29 15:13:37 +0100

    Initial commit: Add hotel_management.py

--- hotel_management.py
+++ hotel_management.py
@@ -1,5 +1,1 @@
-# hotel_managment.py
-print ("Welcome to the Hotel Management Software!")
-git add hotel_management.py
-git commit -m "Initial commit: Add hotel_management.py"
-echo "# My Hotel Management Project"  
+# My Hotel Management Project
