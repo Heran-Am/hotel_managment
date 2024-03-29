4b8f3c811c8b022b039da9e7cdaaffe91e1a19d3
HeranAmbes <149790376+Heran-Am@users.noreply.github.com>
2024-03-29 18:46:34 +0100

    Update layout.tsx

--- src/app/(web)/layout.tsx
+++ src/app/(web)/layout.tsx
@@ -1,6 +1,5 @@
 import type { Metadata } from 'next';
 import { Poppins } from 'next/font/google';
-
 import Header from '@/components/Header/Header';
 import './globals.css';
 import Footer from '@/components/Footer/Footer';
@@ -22,9 +21,7 @@ export const metadata: Metadata = {
 
 export default function RootLayout({
   children,
-}: {
-  children: React.ReactNode;
-}) {
+}: Readonly<{ children: React.ReactNode }>) {
   return (
     <html lang='en'>
       <head>
