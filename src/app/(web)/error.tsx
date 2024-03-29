90031154b14111998243f258a9d9a8f0fc3e05a6
HeranAmbes <149790376+Heran-Am@users.noreply.github.com>
2024-03-29 18:42:23 +0100

    Update error.tsx

--- src/app/(web)/error.tsx
+++ src/app/(web)/error.tsx
@@ -1,19 +1,17 @@
-'use client';
-
 export default function ErrorComponent({
   error,
   reset,
-}: {
+}: Readonly<{
   error: Error & { digest?: string };
   reset: () => void;
-}) {
+}>) {
   return (
     <div className='container mx-auto'>
       <h2 className='font-heading text-red-800 mb-10'>Something went wrong!</h2>
-
       <button onClick={() => reset()} className='btn-primary'>
         Try Again
       </button>
     </div>
   );
 }
+
