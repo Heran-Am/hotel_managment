03d356b2c17bb2e094ebbb03a868ddc66b172979
HeranAmbes <149790376+Heran-Am@users.noreply.github.com>
2024-03-29 16:33:53 +0100

    Update route.ts

--- src/app/api/webhook/route.ts
+++ src/app/api/webhook/route.ts
@@ -1,74 +1,141 @@
-import { NextResponse } from 'next/server';
-import Stripe from 'stripe';
-import { createBooking, updateHotelRoom } from '@/libs/apis';
+import { FC, useState } from 'react';
+import Image from 'next/image';
+import { Image as ImageType } from '@/models/room';
+import { FaArrowLeft, FaArrowRight } from 'react-icons/fa';
+import { MdCancel } from 'react-icons/md';
 
-const checkout_session_completed = 'checkout.session.completed';
-const stripe = new Stripe(process.env.STRIPE_SECRET_KEY as string, {
-  apiVersion: '2023-08-16',
-});
+const HotelPhotoGallery: FC<{ photos: ImageType[] }> = ({ photos }) => {
+  const [currentPhotoIndex, setCurrentPhotoIndex] = useState(0);
+  const [showModal, setShowModal] = useState(false);
 
-export async function POST(req: Request, res: Response) {
-  const reqBody = await req.text();
-  const sig = req.headers.get('stripe-signature');
-  const webhookSecret = process.env.STRIPE_WEBHOOK_SECRET;
-  let event: Stripe.Event;
+  const openModal = (index: number) => {
+    setCurrentPhotoIndex(index);
+    setShowModal(true);
+  };
 
-  try {
-    if (!sig || !webhookSecret) return;
-    event = stripe.webhooks.constructEvent(reqBody, sig, webhookSecret);
-  } catch (error: any) {
-    return new NextResponse(`Webhook Error: ${error.message}`, { status: 500 });
-  }
+  const closeModal = () => setShowModal(false);
 
-  // Load our event
-  switch (event.type) {
-    case checkout_session_completed: {
-      const session = event.data.object;
-      // Destructuring assignment inside block scope
-      const {
-        // @ts-ignore
-        metadata: {
-          adults,
-          checkinDate,
-          checkoutDate,
-          children,
-          hotelRoom,
-          numberOfDays,
-          user,
-          discount,
-          totalPrice,
-        },
-      } = session;
+  const handlePrevious = () => {
+    setCurrentPhotoIndex(prevIndex =>
+      prevIndex === 0 ? photos.length - 1 : prevIndex - 1
+    );
+  };
 
-      // Create booking
-      await createBooking({
-        adults: Number(adults),
-        checkinDate,
-        checkoutDate,
-        children: Number(children),
-        hotelRoom,
-        numberOfDays: Number(numberOfDays),
-        discount: Number(discount),
-        totalPrice: Number(totalPrice),
-        user,
-      });
+  const handleNext = () => {
+    setCurrentPhotoIndex(prevIndex =>
+      prevIndex === photos.length - 1 ? 0 : prevIndex + 1
+    );
+  };
 
-      // Update hotel Room
-      await updateHotelRoom(hotelRoom);
+  const maximumVisiblePhotos = 4;
+  const totalPhotos = photos.length;
+  const displayPhotos = photos.slice(1, maximumVisiblePhotos - 1);
+  const remainingPhotosCount = totalPhotos - maximumVisiblePhotos;
 
-      // Return success response
-      return NextResponse.json('Booking successful', {
-        status: 200,
-        statusText: 'Booking Successful',
-      });
-    }
-    default:
-      console.log(`Unhandled event type ${event.type}`);
-  }
+  return (
+    <div className='container mx-auto'>
+      <div className='grid md:grid-cols-2 relative gap-5 px-3'>
+        <div className='h-[540px] relative rounded-2xl overflow-hidden'>
+          <div className='hidden md:flex justify-center items-center w-full h-full'>
+            <Image
+              src={photos[0].url}
+              alt={`Room Photo ${currentPhotoIndex + 1}`}
+              className='img scale-animation cursor-pointer'
+              width={150}
+              height={150}
+              onClick={openModal.bind(this, 0)}
+            />
+          </div>
+          <div className='md:hidden flex justify-center items-center w-full h-full'>
+            <Image
+              src={photos[currentPhotoIndex].url}
+              alt={`Room Photo ${currentPhotoIndex + 1}`}
+              className='img'
+              width={150}
+              height={150}
+              onClick={openModal.bind(this, 0)}
+            />
+          </div>
+        </div>
+        <div className='md:hidden flex justify-between items-center'>
+          <div className='flex space-x-2'>
+            <FaArrowLeft className='cursor-pointer' onClick={handlePrevious} />
+            <FaArrowRight className='cursor-pointer' onClick={handleNext} />
+          </div>
+          <span>
+            {currentPhotoIndex + 1} / {photos.length}
+          </span>
+        </div>
+        <div className='hidden md:grid grid-cols-2 h-full gap-5'>
+          {displayPhotos.map((photo, index) => (
+            <div
+              key={index}
+              className='cursor-pointer h-64 rounded-2xl overflow-hidden'
+            >
+              <Image
+                width={150}
+                height={150}
+                src={photo.url}
+                alt={`Room Photo ${index + 2}`}
+                className='img scale-animation'
+              />
+            </div>
+          ))}
+          {remainingPhotosCount > 0 && (
+            <div
+              className='cursor-pointer relative h-64 rounded-2xl overflow-hidden'
+              onClick={() => openModal(maximumVisiblePhotos)} // Open modal for remaining photos
+            >
+              <Image
+                width={150}
+                height={150}
+                src={photos[maximumVisiblePhotos - 1].url}
+                alt={`Room Photo ${maximumVisiblePhotos}`}
+                className='img'
+              />
+              <div className='absolute cursor-pointer text-white inset-0 flex justify-center bg-[rgba(0,0,0,0.5)] items-center text-2xl'>
+                + {remainingPhotosCount}
+              </div>
+            </div>
+          )}
+        </div>
+        {showModal && (
+          <div className='fixed top-0 left-0 w-full h-full flex justify-center items-center bg-black bg-opacity-90 z-[55]'>
+            <div className='h-[75vh] w-[320px] md:w-[700px] relative'>
+              <Image
+                src={photos[currentPhotoIndex].url}
+                alt={`Room Photo ${currentPhotoIndex + 1}`}
+                width={150}
+                height={150}
+                className='img'
+              />
+              <div className='flex justify-between items-center py-3'>
+                <div className='flex space-x-2 items-center text-white'>
+                  <FaArrowLeft
+                    className='cursor-pointer'
+                    onClick={handlePrevious}
+                  />
+                  <FaArrowRight
+                    className='cursor-pointer'
+                    onClick={handleNext}
+                  />
+                </div>
+                <span className='text-white text-sm'>
+                  {currentPhotoIndex + 1} / {photos.length}
+                </span>
+              </div>
+              <button
+                className='absolute top-2 right-2 text-white text-lg'
+                onClick={closeModal}
+              >
+                <MdCancel className='font-medium text-2xl text-tertiary-dark' />
+              </button>
+            </div>
+          </div>
+        )}
+      </div>
+    </div>
+  );
+};
 
-  // Return default response
-  return NextResponse.json('Event Received', {
-    status: 200,
-    statusText: 'Event Received',
-  });
-}
+export default HotelPhotoGallery;
