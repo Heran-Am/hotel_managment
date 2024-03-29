cdcf19a05e4a8394e4eaf055a4dbfd5444dcc875
HeranAmbes <149790376+Heran-Am@users.noreply.github.com>
2024-03-29 17:47:50 +0100

    Update ThemeProvider.tsx

--- src/components/ThemeProvider/ThemeProvider.tsx
+++ src/components/ThemeProvider/ThemeProvider.tsx
@@ -1,16 +1,11 @@
 'use client';
-
 import { useEffect, useState } from 'react';
-
 import ThemeContext from '@/context/themeContext';
 
 const ThemeProvider = ({ children }: { children: React.ReactNode }) => {
-  const themeFromStorage: boolean =
-    typeof localStorage !== 'undefined' && localStorage.getItem('hotel-theme')
-      ? JSON.parse(localStorage.getItem('hotel-theme')!)
-      : false;
-
-  const [darkTheme, setDarkTheme] = useState<boolean>(themeFromStorage);
+  const themeFromStorage: boolean | null =
+    typeof localStorage !== 'undefined' ? JSON.parse(localStorage.getItem('hotel-theme')!) : null;
+  const [darkTheme, setDarkTheme] = useState<boolean>(themeFromStorage || false);
   const [renderComponent, setRenderComponent] = useState(false);
 
   useEffect(() => {
