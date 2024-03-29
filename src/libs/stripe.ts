17561cb3d0d7d5659a6091e72d9e3fbc63a188dc
HeranAmbes <149790376+Heran-Am@users.noreply.github.com>
2024-03-29 14:40:56 +0100

    Update stripe.ts

--- src/libs/stripe.ts
+++ src/libs/stripe.ts
@@ -2,12 +2,15 @@ import { Stripe, loadStripe } from '@stripe/stripe-js';
 
 let stripePromise: Promise<Stripe | null>;
 
-export const getStripe = () => {
-  if (!stripePromise) {
-    stripePromise = loadStripe(
-      process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY as string
-    );
+export const getStripe = async () => {
+  try {
+    if (stripePromise === undefined) {
+      stripePromise = loadStripe(process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY as string);
+    }
+    return await stripePromise;
+  } catch (error) {
+    console.error('Error initializing Stripe:', error);
+    throw error; // Rethrow the error to propagate it further if needed
   }
-
-  return stripePromise;
 };
+
