ee70ffb6eaf7f03e07f7fe3f39fa8252c3b6c884
HeranAmbes <149790376+Heran-Am@users.noreply.github.com>
2024-03-29 17:41:24 +0100

    Update RoomReview.tsx

--- src/components/RoomReview/RoomReview.tsx
+++ src/components/RoomReview/RoomReview.tsx
@@ -25,22 +25,20 @@ const RoomReview: FC<{ roomId: string }> = ({ roomId }) => {
 
   return (
     <>
-      {roomReviews &&
-        roomReviews.map(review => (
-          <div
-            className='bg-gray-100 dark:bg-gray-900 p-4 rounded-lg'
-            key={review._id}
-          >
-            <div className='font-semibold mb-2 flex'>
-              <p>{review.user.name}</p>
-              <div className='ml-4 flex items-center text-tertiary-light text-lg'>
-                <Rating rating={review.userRating} />
-              </div>
-            </div>
-
-            <p>{review.text}</p>
-          </div>
-        ))}
+  {roomReviews?.map(review => (
+  <div
+    className='bg-gray-100 dark:bg-gray-900 p-4 rounded-lg'
+    key={review._id}
+  >
+    <div className='font-semibold mb-2 flex'>
+      <p>{review.user?.name}</p>
+      <div className='ml-4 flex items-center text-tertiary-light text-lg'>
+        <Rating rating={review.userRating} />
+      </div>
+    </div>
+    <p>{review.text}</p>
+  </div>
+))}
     </>
   );
 };
