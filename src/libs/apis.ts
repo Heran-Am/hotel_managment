e0eb046697b57cda86fbc592af49cabda13d6d11
HeranAmbes <149790376+Heran-Am@users.noreply.github.com>
2024-03-29 18:19:18 +0100

    Update apis.ts

--- src/libs/apis.ts
+++ src/libs/apis.ts
@@ -135,7 +135,7 @@ export async function checkReviewExists(
 
   const result = await sanityClient.fetch(query, params);
 
-  return;
+  return ? result[0] :null ;
 }
 
 export const updateReview = async ({
